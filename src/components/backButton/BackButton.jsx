16188d3fe79ceae3bf332ff9a2f8ad79d06ddd60
Matteo Ambruschi <matteoambruschi10@gmail.com>
2024-01-31 17:23:25 +0100

    button back gsap

--- src/components/backButton/BackButton.jsx
+++ src/components/backButton/BackButton.jsx
@@ -1,30 +1,41 @@
 import { useNavigate } from "react-router-dom";
 import styles from './backButton.module.css'
-/* import { gsap } from "gsap";
-    
+
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 
-gsap.registerPlugin(ScrollTrigger); */
+gsap.registerPlugin(ScrollTrigger);
 
 function BackButton(){
     const navigate = useNavigate();
+    const buttonRef = useRef();
 
     const handleBack = () => {
         navigate(-1);
       };
 
-/*       gsap.to('backButton', {
-        scrollTrigger: {
-        trigger: 'backButton',
-        start: 'top 80%',
-        end: 'top 30%',
-        markers: true
-        }
-      }) */
+      useGSAP(() => {
+        gsap.to(buttonRef.current, {
+          height: 50,
+          width: 150,
+          fontSize: 17,
+          borderRadius: 8,
+          scrollTrigger: {
+          trigger: buttonRef.current,
+          start: 'top 10%',
+          end: 'bottom -20%',
+          markers: false,
+          scrub: true,
+          }
+        })
+    }, [/* buttonRef */])
 
     return (
         <button
+            ref={buttonRef}
             onClick={() => handleBack()}
             className={styles.backButton}
             >
